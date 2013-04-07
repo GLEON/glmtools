7f84cece9d006122e118f76f6f23c2a994537f23
Jordan S Read <jread@usgs.gov>
2013-04-07 14:53:08 -0500

    Starting to flesh out helper functions

--- Examples/fishDerivatives.r
+++ Examples/fishDerivatives.r
@@ -1,44 +1,104 @@
 # fish code for Gretchen 
 
+
+# -- shared variables --
+timeID  <-  "DateTime"
+
+getGLM  <-  function(folder){
+  source('../Source/GLMnetCDF.R')
+  GLM  <-	resampleGLM(fileName='output.nc',folder=folder)
+  return(GLM)
+}
+
 subsetTime <- function(GLM,startDate,stopDate){
   # gets rid of GLM simulation results that are < startDate and > stopDate
-  
+  # startDate and stopDate must be of type "Date"
+  dates <-  GLM[timeID]
+  useI  <-  (dates >= startDate) & (dates <= stopDate)
+  GLM <-  GLM[useI,]
+  return(GLM)
+}
+
+getTemp   <- function(GLM){
+  drops <- c(timeID)
+  temp <- GLM[,!(names(GLM) %in% drops)]
+  return(temp)
+}
+
+getDailyTempMax <- function(GLM){
+  temp <- getTemp(GLM)
+  dailyTempMax <-  apply(temp,1,function(x) max(x,na.rm=TRUE))
+  return(dailyTempMax)
+}
+
+getDailyTempMin <- function(GLM){
+  temp <- getTemp(GLM)
+  dailyTempMin <-  apply(temp,1,function(x) min(x,na.rm=TRUE))
+  return(dailyTempMin)
 }
 
 getTempMax <- function(GLM){
+  dailyTempMax <-  getDailyTempMax(GLM)
+  tempMax <-  max(dailyTempMax)
   return(tempMax)
 }
 
 getTempMin <- function(GLM){
+  dailyTempMin <-  getDailyTempMin(GLM)
+  tempMin <-  min(dailyTempMin)
   return(tempMin)
 }
 
+getDaysAboveT <- function(GLM,temperature,anyDep=TRUE){
+  # ANY or ALL depths, default is ANY
+  if (anyDep==TRUE){refTemp  <-  getDailyTempMax(GLM)}
+  else {refTemp <-  getDailyTempMin(GLM)}
 
-getDaysAboveT <- function(GLM,temperature){
+  tempAboveCount  <-  sum(refTemp > temperature,na.rm=TRUE)
   return(tempAboveCount)
 }
 
-getDaysBelowT <- function(GLM,temperature){
+getDaysBelowT <- function(GLM,temperature,anyDep=TRUE){
+  # ANY or ALL depths, default is ANY
+  if (anyDep==TRUE){refTemp  <-  getDailyTempMin(GLM)}
+  else {refTemp <-  getDailyTempMax(GLM)}
+  
+  tempBelowCount  <-  sum(refTemp < temperature,na.rm=TRUE)
   return(tempBelowCount)
 }
 
-getDaysBetweenT <-  function(GLM,temperatureLow,temperatureHigh){
+getDaysBetweenT <-  function(GLM,temperatureLow,temperatureHigh,anyDep=TRUE){
+  # ANY or ALL depths, default is ANY
+  temp <- getTemp(GLM)
+  if (anyDep==TRUE){
+    tempRangeCount  <-  sum(apply(temp,1,function(x) any(x>=temperatureLow) & any(x<=temperatureHigh)))
+  }
+  else{
+    tempRangeCount  <-  sum(apply(temp,1,function(x) all(x>=temperatureLow,na.rm=TRUE) 
+      & all(x<=temperatureHigh,na.rm=TRUE)))
+  }
   return(tempRangeCount)
 }
 
 getSurfaceT <- function(GLM){
+  surfaceTemp <- apply(temp,1,function(x) x[max(which(!is.na(x)))])
   return(surfaceTemp)
 }
 
-getIceOffDate <- function(GLM,year){
+getBottomT <- function(GLM){
+  bottomTemp <- apply(temp,1,function(x) x[min(which(!is.na(x)))])
+  return(bottomTemp)
+}
+
+getIceOffDate <- function(GLM){
   return(iceOffDOY)
 }
 
-getIceOnDate  <-  function(GLM,year){
+getIceOnDate  <-  function(GLM){
   return(iceOnDOY)
 }
 
-getLastDayAboveT <-  function(GLM){
+getLastDayAboveT <-  function(GLM,temperature,anyDep=TRUE){
   return(lastDOYabove)
 }
 

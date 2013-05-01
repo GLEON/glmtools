cb4202f8ed72b9fb6f867c02b558d7c03da0d7b0
Jordan S Read <jread@usgs.gov>
2013-05-01 17:03:05 -0500

    function subsampleGLM for compare w/ field

--- Source/GLMnetCDF.R
+++ Source/GLMnetCDF.R
@@ -21,12 +21,18 @@ getIceGLMnc <-  function(GLMnc){
 	return(ice)
 }
 
-subsampleGLM	<-	function(GLM, sampleTime, sampleElev){
+subsampleGLM	<-	function(GLM, sampleTime, sampleDepths){
 	# sample at depths of 'sampleElev' at time 'sampleTime'
+	glmElev	<-	getElevGLM(GLM)
 	surfaceElevs <-  getSurfaceElevGLM(GLM)
-	
-	
-	return(wtr)
+	dates	<-	GLM$DateTime
+	time	<-	as.POSIXct(sampleTime)
+	uIndx	<-	which.min(abs(dates-time))	# NEED good way to interpolate temporally to get exact time...
+	interpElevs	<-	surfaceElevs[uIndx]-sampleDepths	# now are elevations
+	drops <- c(timeID)
+	temp <- as.numeric(GLM[uIndx,!(names(GLM) %in% drops)])
+	wtr	<-	approx(glmElev,temp,xout=interpElevs)
+	return(wtr$y)
 }
 
 resampleGLM	<-	function(GLMnc, lyrDz=0.25){

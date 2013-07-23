b90a55a6d9eb020b062e29ef6f0f58423a7cc265
Jordan S Read <jread@usgs.gov>
2013-07-23 11:22:31 -0500

    working out nml read and write

--- Source/GLMnml.R
+++ Source/GLMnml.R
@@ -1,6 +1,6 @@
 # ------Helper functions for interacting with glm.nml files-----
 # ------Jordan and Luke 2013
-
+commentStr	<-	'!'
 
 getNML	<-	function(fileName='glm.nml',folder='../Data/'){
 	# skip all commented lines, return all variables and associated values
@@ -9,24 +9,40 @@ getNML	<-	function(fileName='glm.nml',folder='../Data/'){
 	c <- file(fileN,"r") 
 	fileLines <- readLines(c)
 	close(c)
-	ignoreLn	<-	sort(c(grep("!",fileLines),grep("/",fileLines),grep("&",fileLines)))
+	lineStart	<-	substr(fileLines,1,1)
+	ignoreLn	<-	lineStart==commentStr | fileLines==""
+	lineStart	<-	lineStart[!ignoreLn]
+	fileLines	<-	fileLines[!ignoreLn]
+	# find all lines which start with "&" * requires FIRST char to be value
 	
-	# will still include blank lines
-	nml	<-	list()
-	for (i in 1:length(fileLines)){
-		if (!any(i==ignoreLn)){
-			if (nchar(fileLines[i])>2){	# get rid of blank lines **issue w/ data format!**
-				# replace blanks
-				fileLines[i]	<-	gsub("\t","",gsub(" ","",fileLines[i]))
-				nml	<-	buildNML(nml,fileLines[i])
-			}
+	
+	lineIdx		<- seq(1,length(lineStart))
+	blckOpen	<-	lineIdx[lineStart=="&"]
+	blckClse	<-	lineIdx[lineStart=="/"]
+	
+	
+	nml <- list()
+	for (i in 1:length(blckOpen)){
+		blckName	<-	substr(fileLines[blckOpen[i]],2,nchar(fileLines[blckOpen[i]]))
+		oldNms	<-	names(nml)
+		nml[[i]]	<-	list()
+		names(nml)	<-	c(oldNms,blckName)
+		for (j in (blckOpen[i]+1):(blckClse[i]-1)){
+			textLine	<-	gsub("\t","",gsub(" ","",fileLines[j]))
+			if(substr(textLine,1,1)!=commentStr){ 
+				# else, line is commented out
+				lineVal	<-	buildVal(textLine)
+				nml[[i]]	<-	c(nml[[i]],lineVal)
+				}
 		}
-	}	
+	}
 	return(nml)
 }
-
-buildNML	<-	function(nml,textLine){
+buildVal	<-	function(textLine){
 	#-----function appends nml list with new values-----
+	# remove all text after comment string
+	textLine	<-	strsplit(textLine,commentStr)[[1]][1]
+	
 	if (!any(grep("=",textLine))){stop(c("no hanging lines allowed in .nml, used ",textLine))}
 	params	<-	strsplit(textLine,"=") # break text at "="
 	parNm	<-	params[[1]][1]
@@ -34,6 +50,11 @@ buildNML	<-	function(nml,textLine){
 	# figure out what parval is...if string, remove quotes and keep as string
 	# ***for boolean text, use "indentical" so that 0!= FALSE
 	# can be: string, number, comma-sep-numbers, or boolean
+	
+	# special case for date:
+	if (nchar(parVl>17) & substr(parVl,14,14)==':' & substr(parVl,17,17)==':'){
+		parVl<-paste(c(substr(parVl,1,11),' ',substr(parVl,12,nchar(parVl))),collapse='')
+		}
 	if (any(grep("'",parVl))){
 		parVl	<-	gsub("'","",parVl)
 	}else if (any(grep(".true.",parVl))){
@@ -45,12 +66,11 @@ buildNML	<-	function(nml,textLine){
 	}else {	# test for number
 		parVl	<-	as.numeric(parVl)
 	}
-	addI	<-	length(nml)+1
-	oldNms	<-	names(nml)
-	nml[[addI]]	<-	parVl
-	names(nml)	<-	c(oldNms,parNm)
-	return(nml)
+	lineVal	<-	list(parVl)
+	names(lineVal)	<-	parNm
+	return(lineVal)
 }
+
 setNML	<-	function(nml,argName,argVal){
 	nml[argName]	<-	argVal
 	return(nml)
@@ -58,32 +78,24 @@ setNML	<-	function(nml,argName,argVal){
 
 writeNML	<-	function(nml,fileName='glm.nml',folder='../Data/'){
 	sink(paste(c(folder,fileName),collapse=''))
-	for (i in 1:length(names(nml))){
-		cat(names(nml[i]))
-		cat('=')
-		if (length(nml[[i]]>1)){
-			writer	<-	paste(nml[[i]],colapse=',')
-		} else {write	<-	nml[[1]]}
-		cat(nml[[i]])
+	for (i in 1:length(names(nml))){ # these are the blocks
+		blckNm	<-	names(nml)[i]
+		cat("&")
+		cat(blckNm)
 		cat('\n')
+		blckList	<-	nml[[i]]
+		for (j in 1:length(names(blckList))){
+			cat('   ')
+			cat(names(blckList)[j])
+			cat(' = ')
+			if (length(blckList[[j]]>1)){
+				writer	<-	paste(blckList[[j]],colapse=',')
+			} else {writer	<-	blckList[[j]]}
+			cat(writer)
+			cat('\n')
+		}
+		cat('/\n')
 	}	
 	sink()
-}
-
-getLakeName	<-	function(nml){
-	lakeName	<-	nml$lake_name
-	return(lakeName)
-}
-
-getMaxElevation	<-	function(nml){
-	maxElevation	<-	nml$crest_elev
-	return(maxElevation)
-}
-
-getMaxDepth	<-	function(nml){
-	mxElv	<-	nml$crest_elev
-	mnElv	<-	nml$base_elev
-	maxDepth	<-	mxElv-mnElv
-	return(maxDepth)
 }
 

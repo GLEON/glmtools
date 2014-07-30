2c1659314535c36e39a38c60f6b00b7939e2a2d0
Jordan S Read <jread@usgs.gov>
2014-07-30 11:33:10 -0500

    resample_to_field method added and tested.

--- R/resample_to_field.R
+++ R/resample_to_field.R
@@ -1,7 +1,7 @@
 #'@title match GLM water temperatures with field observations
 #'@param nc_file a string with the path to the netcdf output from GLM
 #'@param field_file a string with the path to the field observation file
-#'@return a data.frame with DateTime and temperature at depth 
+#'@return validation a data.frame with DateTime and temperature at depth 
 #'@keywords methods
 #'@seealso \link{resample_time}, \link{get_temps}
 #'@author
@@ -25,5 +25,29 @@ resample_to_field <- function(nc_file, field_file){
   
   unq_z <- unique(field_obs$Depth) # levels?
   
-  return(df)
+  # build water temp data.frame
+  wTemps <- get_temp(file = nc_file, reference = 'surface', z_out = unq_z)
+  wTemps <- trunc_time(wTemps, start_date = min(field_obs$DateTime), stop_date = max(field_obs$DateTime))
+  wTemps <- resample_time(df = wTemps, t_out = unique(field_obs$DateTime), method = 'match', precision = 'day')
+  match_vals <- pivot_match(wTemps, time = field_obs$DateTime, depth = field_obs$Depth)
+  validation <- data.frame('DateTime' = field_obs$DateTime, 
+                           'Depth' = field_obs$Depth, 
+                           'Observed_wTemp' = field_obs$wTemp,
+                           'Modeled_wTemp' = match_vals)
+  
+  return(validation)
+}
+
+pivot_match <- function(df, time, depth){
+  
+  #time and depth are 1D vectors (long-form tables)
+  match_out <- vector(length = length(time))
+  
+  for (j in 1:length(time)){
+    dt_match <- time[j]
+    dp_match <- depth[j]
+    col_nm <- match(paste0('wtr_', dp_match), names(df)) # this is not a numeric test!!! fix this (wtr_9.340 != 9.34)
+    match_out[j] <- df[df$DateTime==dt_match, col_nm]
+  }
+  return(match_out)
 }

22eaab3ab3664e0ff3fcced466c146ead683a311
Jordan S Read <jread@usgs.gov>
2014-07-30 08:20:54 -0500

    added t_out arg to be passed to resample_time

--- R/get_temp.R
+++ R/get_temp.R
@@ -8,20 +8,28 @@
 #'@param file a string with the path to the netcdf output from GLM
 #'@param reference a string which specifies the vertical reference ('surface' or 'bottom')
 #'@param z_out a vector of depths for temperature output (in meters)
+#'@param t_out a vector of POSIXct dates for temporal resampling (order is important)
 #'@return a data.frame with DateTime and temperature at depth 
 #'@keywords methods
+#'@seealso \link{resample_time}
 #'@author
 #'Jordan S. Read, Luke A. Winslow
 #'@examples 
 #'file <- system.file('extdata', 'output.nc', package = 'glmtools')
 #'temp_surf <- get_temp(file, reference = 'surface', z_out = c(0,1,2))
 #'temp_bot <- get_temp(file, reference = 'bottom', z_out = c(0,1,2))
+#'
+#'#-- get temporal subset--
+#'t_out <- seq(as.POSIXct("2008-03-01"), as.POSIXct("2008-06-01"), by = 86400)
+#'temp_surf <- get_temp(file, reference = 'surface', z_out = 0, t_out = t_out)
+#'plot(temp_surf)
 #'@import ncdf4
 #'@export
-get_temp <-  function(file, reference = 'bottom', z_out){
+get_temp <-  function(file, reference = 'bottom', z_out, t_out = NULL){
   if (reference!='bottom' & reference!='surface'){
     stop('reference input must be either "surface" or "bottom"')
   }
+
   
   glm_nc <- get_glm_nc(file)
   
@@ -29,6 +37,7 @@ get_temp <-  function(file, reference = 'bottom', z_out){
   elev <- ncvar_get(glm_nc, "z" )
   temp <- ncvar_get(glm_nc, "temp")
   time <- get_time(glm_nc)
+  
   nc_close(glm_nc)
   
   if (reference=='surface') {
@@ -58,9 +67,9 @@ get_temp <-  function(file, reference = 'bottom', z_out){
   for (tme in 1:num_step){
     if (reference == 'surface') elevs_out <- elev_surf[tme, 2] - z_out
     if (is.null(nrow(temp))){ # handle single depth layer of model
-      temp_out[tme, ] <- depth_resample(elevs=elev[tme], temps=temp[tme], elevs_out)
+      temp_out[tme, ] <- resample_depth(elevs=elev[tme], temps=temp[tme], elevs_out)
     } else {
-      temp_out[tme, ] <- depth_resample(elevs=elev[, tme], temps=temp[, tme], elevs_out)
+      temp_out[tme, ] <- resample_depth(elevs=elev[, tme], temps=temp[, tme], elevs_out)
     }
     
   }
@@ -76,11 +85,13 @@ get_temp <-  function(file, reference = 'bottom', z_out){
   }
   names(glm_temp)<- frameNms
 
+  glm_temp <- resample_time(glm_temp, t_out)
+  
   return(glm_temp)
 }
 
 
-depth_resample <- function(elevs, temps, elevs_out){
+resample_depth <- function(elevs, temps, elevs_out){
   #strip out NAs
   rmv_i <- temps>=1e30 | elevs>=1e30
   elevs <- elevs[!rmv_i]

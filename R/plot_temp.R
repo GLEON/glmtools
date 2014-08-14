bb1d377a086455b91e7fc20d9e09e4033bb168c1
Jordan S Read <jread@usgs.gov>
2014-08-14 12:16:12 -0500

    v0.2.3 plotting support for heatmap figures

--- R/plot_temp.R
+++ R/plot_temp.R
@@ -1,6 +1,7 @@
 #'@title plot water temperatures from a GLM simulation
 #'@param file a string with the path to the netcdf output from GLM
 #'@param refences a string for 'surface' or 'bottom'
+#'@param num_cells number of vertical cells to use for heatmap
 #'@keywords methods
 #'@seealso \link{get_temp}
 #'@author
@@ -9,12 +10,12 @@
 #'file <- system.file('extdata', 'output.nc', package = 'glmtools')
 #'plot_temp(file = file)
 #'@export
-plot_temp <- function(file, reference = 'surface'){
+plot_temp <- function(file, reference = 'surface', num_cells = 100){
 
   surface <- get_surface_height(file)
   max_depth <- max(surface[, 2])
   min_depth <- 0
-  z_out <- seq(min_depth, max_depth,length.out = 100)
+  z_out <- seq(min_depth, max_depth,length.out = num_cells)
   temp <- get_temp(file, reference = reference, z_out)
   palette <- colorRampPalette(c("violet","blue","cyan", "green3", "yellow", "orange", "red"), 
                               bias = 1, space = "rgb")
@@ -33,6 +34,7 @@ plot_temp <- function(file, reference = 'surface'){
                   levels= levels,
                   col=colors)
   
+  axis_layout(xaxis, yaxis) #doing this after heatmap so the axis are on top
   color_key(levels, colors, subs=seq(4,32,2))
   dev.off()
 }
@@ -47,6 +49,10 @@ plot_layout <- function(xaxis, yaxis){
        xlab=xaxis$x_lab, ylab=' ',
        frame=FALSE,axes=F,xaxs="i",yaxs="i")
   
+  
+}
+
+axis_layout <- function(xaxis, yaxis){
   # x axis
   axis(side = 1, labels=format(xaxis$vis_time, xaxis$time_form), at = xaxis$vis_time, tck = -0.01, pos = yaxis$lim[1])
   axis(side = 3, labels=NA, at = xaxis$lim, tck = 0)
@@ -56,7 +62,4 @@ plot_layout <- function(xaxis, yaxis){
   axis(side = 2, at = mean(yaxis$lim), tck = 0,  labels=yaxis$title)
   par(mgp=ol_par)
   axis(side = 4, labels=NA, at = yaxis$lim, tck = 0)
-  
-  
-  
 }

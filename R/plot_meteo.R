775fb2c1d15c178a4257a2167f0cd5f2f05bcbc8
Jordan S Read <jread@usgs.gov>
2014-10-18 20:27:57 -0500

    fig_path error added

--- R/plot_meteo.R
+++ R/plot_meteo.R
@@ -1,6 +1,6 @@
 #'@title plot meterological drivers from a csv file
 #'@param nml_file a string with the path to the nml file for the simulation 
-#'@param fig_path F if plot to screen, string path if save plot as .png
+#'@param fig_path FALSE if plot to screen, string path if save plot as .png
 #'@keywords methods
 #'@seealso \link{read_nml}
 #'@author
@@ -11,11 +11,14 @@
 #'plot_meteo(nml_file)
 #'@import ncdf4
 #'@export
-plot_meteo <- function(nml_file, fig_path = F){
+plot_meteo <- function(nml_file, fig_path = FALSE){
   glm_nml <- read_nml(nml_file)
   met_file <- get_nml_value(glm_nml,'meteo_fl') 
   met_path <- file.path(dirname(nml_file),met_file)
 
+  if (fig_path){
+    stop('figure save not currently supported. Use fig_path = FALSE')
+  }
   if (!file.exists(met_path)){stop(paste0("nml_file points to a meteo file that doesn't exist:\n",met_path))}
   
   meteo <- read.csv(file = met_path)

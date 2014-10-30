bc4a842c2c702e07888e0973be30e34b18bd40c5
Jordan S Read <jread@usgs.gov>
2014-10-30 13:37:33 -0400

    glm2_test.nml

--- R/write_nml.R
+++ R/write_nml.R
@@ -11,7 +11,7 @@
 #'@seealso \link{get_nml_value}, \link{read_nml}
 #'@examples 
 #'glm_nml <- read_nml()
-#'write_nml(glm_nml, file = '../glm_test.nml')
+#'write_nml(glm_nml, file = '../glm2_test.nml')
 #'@export
 write_nml  <-	function(glm_nml,file){
   sink(file)

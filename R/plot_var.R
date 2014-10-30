d96b19d768225ff3ecb58a9bdbfad23207f3efcd
Jordan S Read <jread@usgs.gov>
2014-10-30 14:01:06 -0400

    e.g. added, fail on 1D plot try

--- R/plot_var.R
+++ R/plot_var.R
@@ -1,4 +1,4 @@
-#'@title plot water temperatures from a GLM simulation
+#'@title plot variable from a GLM simulation
 #'@param file a string with the path to the netcdf output from GLM
 #'@param var_name a string for a valid variable name
 #'@param col_lim a numeric array of length 2 that specifies the min and max value for color bar
@@ -11,6 +11,14 @@
 #'@seealso \link{get_temp}
 #'@author
 #'Jordan S. Read, Luke A. Winslow
+#'@examples
+#'\dontrun{
+#'plot_var(file = fabm_sim_nc,
+#'var_name = 'aed_oxygen_oxy', 
+#'col_lim=c(0,400),
+#'bar_title = 'Dissolved Oxygen', 
+#'fig_path = 'aed_out.png')
+#'}
 #'@export
 plot_var <- function(file, var_name, col_lim, reference = 'surface', num_cells = 100, fig_path = F, add = F, bar_title = NULL){
   
@@ -18,14 +26,17 @@ plot_var <- function(file, var_name, col_lim, reference = 'surface', num_cells =
   max_depth <- max(surface[, 2])
   min_depth <- 0
   z_out <- seq(min_depth, max_depth,length.out = num_cells)
-  temp <- get_var(file, reference = reference, z_out, var_name=var_name)
+  variable_df <- get_var(file, reference = reference, z_out, var_name=var_name)
+  if (ncol(variable_df) == 2){
+    stop('plot_var() not yet supported for 1D output variables')
+  }
   palette <- colorRampPalette(c("violet","blue","cyan", "green3", "yellow", "orange", "red"), 
                               bias = 1, space = "rgb")
   levels <- seq(col_lim[1], col_lim[2], by = 1)
   col_subs <- unique(floor(seq(col_lim[1], col_lim[2]-1, length.out = 15)))
   colors <- palette(n = length(levels)-1)
-  dates <- temp[, 1]
-  wtr_temps <- data.matrix(temp[, -1])
+  dates <- variable_df[, 1]
+  matrix_var <- data.matrix(variable_df[, -1])
   xaxis <- get_xaxis(dates)
   yaxis <- get_yaxis_2D(z_out, reference)
   
@@ -35,7 +46,7 @@ plot_var <- function(file, var_name, col_lim, reference = 'surface', num_cells =
   
   
   plot_layout(xaxis, yaxis, add)
-  .filled.contour(x = dates, y = z_out, z = wtr_temps,
+  .filled.contour(x = dates, y = z_out, z =matrix_var,
                   levels= levels,
                   col=colors)
   

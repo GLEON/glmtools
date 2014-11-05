198a34c8ea26da21e55982e35803cdc68c59782b
Jordan S Read <jread@usgs.gov>
2014-11-05 12:24:19 -0600

    fix for NA all. remove error trigger

--- R/compare_to_field.R
+++ R/compare_to_field.R
@@ -78,28 +78,28 @@ compare_to_field <- function(nc_file, field_file, nml_file, metric, as_value = F
     mod_list <- list(wtr=temp_mod[!rmv_i], depths = depths[!rmv_i], bthA = bthA, bthD = bthD)
     obs_list <- list(wtr=temp_obs[!rmv_i], depths = depths[!rmv_i], bthA = bthA, bthD = bthD)
     use_names <- names(mod_list) %in% names(formals(metric)) # test to only use list elements that are inluded in the function args
-    mod_num <- do.call(get(metric), mod_list[use_names]) 
-    obs_num <- do.call(get(metric), obs_list[use_names]) 
+    if (sum(rmv_i) == length(rmv_i)){
+      mod_num <- NA
+      obs_num <- NA
+    } else {
+      mod_num <- do.call(get(metric), mod_list[use_names]) 
+      obs_num <- do.call(get(metric), obs_list[use_names]) 
+    }
+    
     
     if (as_mat == TRUE) { # ~!!! first date as single value will not be properly handled. !!!
-      if (as_value == TRUE){
-        stop(paste('metric',metric,'is not supported for as_value=TRUE output. 
-                   Only metrics that output a single value per profile are supported. 
-                   Try as_value=FALSE for RMSE.'))
-      }
       if (j == 1){
-        # if as_mat, need a much larger matrix to support. Buffer and fil
+        # if as_mat, need a much larger matrix to support. Buffer and fill
         cnt = 1
         mod_metric <- vector(length = length(un_dates)*200)*NA
         obs_metric <- mod_metric
       } 
       mod_metric[cnt:(cnt+length(mod_num)-1)] = mod_num
       obs_metric[cnt:(cnt+length(mod_num)-1)] = obs_num
-      cat(j);cat(' of '); cat(length(un_dates))
       cnt = 1+length(mod_num)
     } else {
-      mod_metric[j] <- do.call(get(metric), mod_list[use_names]) 
-      obs_metric[j] <- do.call(get(metric), obs_list[use_names]) 
+      mod_metric[j] <- mod_num
+      obs_metric[j] <- obs_num
     }
     
   }

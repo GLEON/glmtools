4fd757cab02eb2fca3c73e14555e9f997a69c691
Jordan S Read <jread@usgs.gov>
2015-01-09 15:48:23 -0600

    new tests for 1D gets

---
+++ tests/testthat/test-get_outputNC.R
@@ -0,0 +1,19 @@
+context("get_wind")
+
+test_that("get_wind", {
+  sim_folder <- run_example_sim(verbose = F)
+  nc_file <- file.path(sim_folder, 'output.nc')
+  wnd <- get_wind(nc_file)
+  t_out <- as.POSIXct(c("1900-01-01","1900-01-02"))
+  
+  #should be empty
+  expect_warning(get_wind(nc_file, t_out = t_out, method = 'interp'))
+  
+  t_out <- as.POSIXct(c("2011-04-05 08:15", 
+                        "2011-06-14 10:30", "2011-04-05 10:21", 
+                        "2011-07-28 10:00"),tz = 'GMT') 
+  
+  df <- get_wind(nc_file, t_out = t_out, method = 'interp', precision = 'exact')
+  expect_true(all(t_out == df[,1]))
+})
+

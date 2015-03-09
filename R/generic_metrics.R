7e76ec750a47e69b9ecc3bc8352b1eb81835a70a
Jordan S Read <jread@usgs.gov>
2015-03-09 12:45:06 -0500

    v0.6.2 GRAN checks addressed

--- R/generic_metrics.R
+++ R/generic_metrics.R
@@ -1,4 +1,10 @@
 #holder for glmtools metrics that follow rLA conventions
+#'@title mimic rLakeAnalyzer function
+#'@details used to mimic an rLakeAnalyzer function, so that it will 
+#'be recognized by a call to \code{\link{sim_metrics}}
+#'@param wtr a water temperature data.frame
+#'
+#'@seealso \code{\link{sim_metrics}}, \code{\link{compare_to_field}}, \code{\link{validate_sim}}
 #'@export
 water.temperature <- function(wtr){
   wtr

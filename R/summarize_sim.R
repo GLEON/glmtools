8717f04a1f91aa8d44c002ad195678fee8d3a5c5
Jordan S Read <jread@usgs.gov>
2015-03-14 18:55:14 -0500

    deprecating summarize_sim()

--- R/summarize_sim.R
+++ R/summarize_sim.R
@@ -16,30 +16,8 @@
 #'
 #'summarize_sim(nc_file, sim_outputs = c('temp','surface_height','wind'))
 #'@export
-summarize_sim <- function(file, sim_outputs = 'temp', fig_path = FALSE){
+summarize_sim <- function(...){
    
-  def.par <- par(no.readonly = TRUE)
-  
-  if (fig_path){
-    stop('figure save not currently supported. Use fig_path = FALSE')
-  }
-  
-  num_metrics = length(sim_outputs)
-  colbar_layout(num_metrics)
-  
-  for (i in 1:num_metrics){
-    plot_fcn <- paste0('plot_', sim_outputs[i])
-    if(existsFunction(plot_fcn)){
-    	do.call(get(plot_fcn), list('file' = file, 'add' = TRUE))
-    }else{
-    	get_fcn <- paste0('get_', sim_outputs[i])
-    	ts <- do.call(get(get_fcn), list('file' = file))
-    	xaxis <- get_xaxis(ts$DateTime)
-    	yaxis <- get_yaxis(data = ts[, 2], 'title' = names(ts)[2])
-    	plot_layout(xaxis, yaxis, add = T, data = ts)
-    	axis_layout(xaxis, yaxis)
-    	plot(0,NA, axes = F, ylim = c(0,1), xlim = c(0,1), ylab='',xlab='') # fill up colorbar null space
-    }
-  }
-  par(def.par)
+  warning('function is deprecated. Use plot_var.')
+  return(NULL)
 }

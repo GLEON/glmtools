29ae0761b3d024b553a605163f3ac025dba10e0a
hdugan <hilarydugan@gmail.com>
2019-05-27 13:37:02 -0500

    Overhauled plot var functions for both heatmaps and timeseries using ggplot2

--- R/plot_var.R
+++ R/plot_var.R
@@ -1,11 +1,15 @@
 #'Plot variables from a GLM simulation
 #'@param file a string with the path to the netcdf output from GLM
 #'@param var_name a character vector of valid variable names (see \code{\link{sim_vars}})
-#'@param fig_path F if plot to screen, string path if save plot as .png
-#'@param reference 'surface' or 'bottom'. Only used for heatmap plots.
-#' @param col_lim range for heatmap (in units of the variable)
-#' @param palette color palette for heatmap (2-D var) plot if non-default is desired
-#'@param ... additional arguments passed to \code{par()}
+#'@param fig_path Logical; F if plot to screen, string path if save plot as .png
+#'@param reference String; `"surface"` or `"bottom"`. Only used for heatmap plots.
+#'@param legend.title Vector string; Default (`NULL`) will use variable and units from netcdf file
+#'@param interval Positive number indicating the depth interval in meters to interpolate output data. Must be less than max depth of lake. Default = 0.5 m. 
+#'@param text.size Integer; Default is 12. Higher values will increase text size in plot.
+#'@param show.legend Logical; TRUE to show legend (default), FALSE to hide legend
+#'@param legend.position String; Legend position. Default is `"right"`. Options: `"left"`, `"right"`, `"top"`, `"bottom"`
+#'@param plot.title Vector string; Default is no title. 
+
 #'@keywords methods
 #'@seealso \code{\link{get_temp}}, \code{\link{sim_var_longname}}, 
 #'\code{\link{sim_vars}}, \code{\link{plot_temp}},  \code{\link{get_var}}
@@ -14,14 +18,17 @@
 #'When creating a heatmap, the output produced by \code{plot_var} is actually a combination of two plots; 
 #'one is a \code{\link[graphics]{.filled.contour}} plot and the other is a legend.
 #'@author
-#'Jordan S. Read, Luke A. Winslow
+#'Jordan S. Read, Luke A. Winslow, Hilary A. Dugan
+#'
 #'@examples
 #'sim_folder <- run_example_sim(verbose = FALSE)
 #'nc_file <- file.path(sim_folder, 'output.nc')
 #'vars <- sim_vars(file = nc_file)
 #'plot_var(nc_file, 'u_mean')
-#'#try alternate palette
-#'plot_var(nc_file, 'u_mean', palette = terrain.colors)
+
+#'#Plotting two variables
+#'plot_var(nc_file, var_name = c('temp','wind'), show.legend = F, text.size = 14, plot.title = c('My Lake: Temp','My Lake: Wind'))
+
 #'
 #'\dontrun{
 #'# need to specify a valid .nc file here: 
@@ -30,31 +37,36 @@
 #'fig_path = 'aed_out.png')
 #'}
 #'@importFrom grDevices dev.off
+#'@importFrom gridExtra grid.arrange
 #'@export
-plot_var <- function(file='output.nc', var_name, fig_path = FALSE, reference='surface', col_lim, palette, ...){
+plot_var <- function(file='output.nc', var_name, fig_path = FALSE, reference='surface', legend.title = NULL, 
+                     interval = 0.5, text.size = 12, show.legend = TRUE, 
+                     legend.position = 'right', plot.title = NULL) {
   
   heatmaps <- .is_heatmap(file, var_name)
   num_divs <- length(var_name)
   
-  is_multiplot = ifelse(num_divs > 1, TRUE, FALSE)
+  # is_multiplot = ifelse(num_divs > 1, TRUE, FALSE)
   is_heatmap = any(heatmaps)
-  
-  def.par <- par(no.readonly = TRUE)
-  gen_default_fig(filename = fig_path, num_divs=num_divs, ...)
-  
-  # -- set up plot layout
-  .stacked_layout(is_heatmap, num_divs)
-  
+
   # iterate through plots
+  h = list() #for ggplots
   for (j in 1:num_divs){
     if (heatmaps[j]){
-      .plot_nc_heatmap(file, var_name[j], reference, col_lim=col_lim, palette=palette)
+        h[[j]] = .plot_nc_heatmap(file = file, var_name = var_name[j], reference = reference,
+                                  legend.title = legend.title[j], interval=interval, text.size = text.size, 
+                                  show.legend = show.legend, legend.position = legend.position, 
+                                  plot.title = plot.title[j])
     } else {
-      .plot_nc_timeseries(file, var_name[j])
+      h[[j]] = .plot_nc_timeseries(file = file, var_name = var_name[j], 
+                                   plot.title = plot.title[j], text.size = text.size)
+                                   
       if(is_heatmap) .plot_null() # to fill up the colormap div
     }
   }
-
+  grid.arrange(grobs = h, ncol = 1)
+  
+  # Saving plot 
   if (is.character(fig_path)){
     fg <- dev.off()
   } else {
@@ -63,3 +75,4 @@ plot_var <- function(file='output.nc', var_name, fig_path = FALSE, reference='su
     layout(1)
   }
 }
+
--- R/timeseries_plots.R
+++ R/timeseries_plots.R
@@ -1,57 +1,59 @@
+#' @importFrom tidyr gather
+#' @import ggplot2
+#' @import dplyr
 
-.plot_nc_heatmap <- function(file, var_name, reference, num_cells=100, palette, ...){
+.plot_nc_heatmap <- function(file, var_name, reference, legend.title , interval,
+                             text.size, show.legend, legend.position, plot.title) {
   
   surface <- get_surface_height(file)
   max_depth <- max(surface[, 2])
   min_depth <- 0
-  z_out <- seq(min_depth, max_depth,length.out = num_cells)
-  data <- get_var(file, z_out = z_out, var_name = var_name, reference = reference)
-  title = .unit_label(file, var_name)
-  .plot_df_heatmap(data, title, num_cells, palette, ...)
+  z_out <- seq(min_depth, max_depth,by = interval) # Set plotting interval
+  # Get data from .nc file
+  data = get_var(file, var_name = var_name, z_out = z_out,reference = reference)
+  # Get units
+  units = sim_var_units(file, var_name = var_name)
+  
+  names.df = data.frame(names = names(data)[-1], depth.numeric = z_out, stringsAsFactors = F)
+  dataLong = gather(data = data,key = depth, value = var,-DateTime) %>%
+    left_join(names.df, by = c('depth' = 'names'))
+  
+  if(is.null(legend.title)) {
+    legend.title = .unit_label(file, var_name)
+  }
+  .plot_df_heatmap(dataLong, legend.title, text.size, show.legend, legend.position, plot.title)
 }
 
-#' @importFrom graphics .filled.contour
-#' @importFrom grDevices colorRampPalette 
-#' @importFrom utils head
-.plot_df_heatmap <- function(data, bar_title, num_cells, palette, title_prefix=NULL, overlays=NULL, xaxis=NULL, col_lim){
+
+.plot_df_heatmap <- function(dataLong, legend.title, text.size, 
+                             show.legend, legend.position, plot.title) {
+                             
+                             # num_cells, palette, title_prefix=NULL, overlays=NULL, xaxis=NULL, col_lim){
   
-  z_out <- rLakeAnalyzer::get.offsets(data)
-  reference = ifelse(substr(names(data)[2],1,3) == 'elv', 'bottom', 'surface')
-  
-  if (missing(col_lim))
-    col_lim = range(data[, -1], na.rm = TRUE)
-  if (missing(palette))
-    palette <- colorRampPalette(c("violet","blue","cyan", "green3", "yellow", "orange", "red"), 
-                                bias = 1, space = "rgb")
-  
-  col_subs <- head(pretty(col_lim, 6), -1)
-  levels <- sort(unique(c(col_subs, pretty(col_lim, 15))))
-  colors <- palette(n = length(levels)-1)
-  dates <- data[, 1]
-  matrix_var <- data.matrix(data[, -1])
-  if(is.null(xaxis)){
-  	xaxis <- get_xaxis(dates)
-  }
-  
-  yaxis <- get_yaxis_2D(z_out, reference, prefix=title_prefix)
-  plot_layout(xaxis, yaxis, add=TRUE)
-  .filled.contour(x = dates, y = z_out, z =matrix_var,
-                  levels= levels,
-                  col=colors)
-  overlays # will plot any overlay functions
-  axis_layout(xaxis, yaxis) #doing this after heatmap so the axis are on top
-  
-  color_key(levels, colors, subs=col_subs, col_label = bar_title)
+  h1 = ggplot(data = dataLong, aes(DateTime, depth.numeric)) +
+  geom_raster(aes(fill = var), interpolate = F, hjust = 0.5, vjust = 0.5, show.legend = show.legend) +
+  scale_y_reverse(expand = c(0.01,0.01)) +
+  scale_x_datetime(expand = c(0.01,0.01)) +
+  scale_fill_viridis_c(alpha = 0.95, option = 'plasma') +
+  ylab('Depth (m)') + xlab('Date') +
+  labs(fill = legend.title, title = plot.title) +
+  theme_bw(base_size = text.size) +
+  theme(legend.position = legend.position)
 }
 
 #' @importFrom graphics points
-.plot_nc_timeseries <- function(file, var_name){
+.plot_nc_timeseries <- function(file, var_name, text.size, plot.title){
   
-  ylab = .unit_label(file, var_name)
-  variable_df <- get_var(file, var_name=var_name)
-  xaxis <- get_xaxis(variable_df[,1])
-  yaxis <- get_yaxis(variable_df[,2], title = ylab)
-  plot_layout(xaxis, yaxis, add=TRUE)
-  points(variable_df)
-  axis_layout(xaxis, yaxis)
+  # Get data from .nc file
+  data = get_var(file, var_name = var_name) %>% 
+    mutate(var_name = as.numeric(get(var_name)))
+  
+  ylab.title = .unit_label(file, var_name)
+  
+  h1 = ggplot(data = data, aes(x = DateTime, y = var_name)) + geom_point(alpha = 0.8) +
+    xlab('Date') + ylab(ylab.title) +
+    scale_y_continuous(expand = c(0.01,0.01)) +
+    scale_x_datetime(expand = c(0.01,0.01)) +
+    labs(title = plot.title) +
+    theme_bw(base_size = text.size)
 }
